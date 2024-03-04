2bbb8ab096f2f3df70bae9e23849cf21e97982e9
02_bao <104081842+02bao@users.noreply.github.com>
2024-03-04 14:24:58 +0700

    bug in 4h39p

--- Bulky.DataAccess/Migrations/20240303053518_AddCategoryToDb.cs
+++ Bulky.DataAccess/Migrations/20240303053518_AddCategoryToDb.cs
@@ -4,7 +4,7 @@
 
 #pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
 
-namespace BulkyWeb.Migrations
+namespace BuBulky.DataAccesslkyWeb.Migrations
 {
     /// <inheritdoc />
     public partial class AddCategoryToDb : Migration
